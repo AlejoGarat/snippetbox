34445c24dcc6db9aac9a3c66b04d30b551cfa39c
AlejoGarat <alejogaratd@gmail.com>
2023-05-08 16:57:37 -0300

    Chapter 5.4. Catching runtime errors

--- pkg/render.go
+++ pkg/render.go
@@ -1,6 +1,7 @@
 package httphelpers
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"text/template"
@@ -16,10 +17,15 @@ func Render(w http.ResponseWriter, status int, page string, templateCache map[st
 		return
 	}
 
-	w.WriteHeader(status)
+	buf := new(bytes.Buffer)
 
-	err := ts.ExecuteTemplate(w, "base", data)
+	err := ts.ExecuteTemplate(buf, "base", data)
 	if err != nil {
 		ServerError(w, err)
+		return
 	}
+
+	w.WriteHeader(status)
+
+	buf.WriteTo(w)
 }
