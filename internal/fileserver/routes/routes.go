68f4a8c90fe30df5606674707d69c9351cac049c
AlejoGarat <alejogaratd@gmail.com>
2023-05-12 14:17:57 -0300

    Chapter 13.1.Using embedded files

--- internal/fileserver/routes/routes.go
+++ internal/fileserver/routes/routes.go
@@ -4,15 +4,16 @@ import (
 	"net/http"
 
 	"github.com/AlejoGarat/snippetbox/pkg/middlewares"
+	"github.com/AlejoGarat/snippetbox/ui"
 	"github.com/alexedwards/scs/v2"
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
 func MakeRoutes(router *httprouter.Router, sessionManager *scs.SessionManager) {
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.FS(ui.Files))
 	mids := alice.New(sessionManager.LoadAndSave, middlewares.LogRequest, middlewares.LogRequest, middlewares.RecoverPanic)
-	router.Handler(http.MethodGet, "/static/",
+	router.Handler(http.MethodGet, "/static/*filepath",
 		mids.Then(
 			middlewares.SecureHeaders(
 				http.StripPrefix("/static", fileServer).ServeHTTP,
